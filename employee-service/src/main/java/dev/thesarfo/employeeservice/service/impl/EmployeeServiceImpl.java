4342561e8f0f96564d06d56912826d04b2d815fe
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-08-08 11:30:40 +0000

    retry pattern implementation

--- employee-service/src/main/java/dev/thesarfo/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/dev/thesarfo/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,13 +8,18 @@ import dev.thesarfo.employeeservice.repository.EmployeeRepository;
 import dev.thesarfo.employeeservice.service.APIClient;
 import dev.thesarfo.employeeservice.service.EmployeeService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     private EmployeeRepository employeeRepository;
     // private RestTemplate restTemplate;
@@ -41,10 +46,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         );
     }
 
-    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+
+    //@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
 
+        LOGGER.info("Inside getEmployeeById method");
         Employee employee = employeeRepository.findById(employeeId).get();
 
 //        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
@@ -74,7 +82,10 @@ public class EmployeeServiceImpl implements EmployeeService {
         return apiResponseDto;
     }
 
+    // this is our fallback method for circuit breaking
     public APIResponseDto getDefaultDepartment(Long employeeId, Exception e){
+
+        LOGGER.info("Inside getDefaultDepartment method");
         Employee employee = employeeRepository.findById(employeeId).get();
 
         DepartmentDto departmentDto = new DepartmentDto();
