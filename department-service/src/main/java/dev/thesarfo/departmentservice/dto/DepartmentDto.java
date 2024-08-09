cd2948ca6a08e8ad261a051c636519d7d6f13e05
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-08-09 19:19:37 +0000

    customize api docs

--- department-service/src/main/java/dev/thesarfo/departmentservice/dto/DepartmentDto.java
+++ department-service/src/main/java/dev/thesarfo/departmentservice/dto/DepartmentDto.java
@@ -1,17 +1,31 @@
 package dev.thesarfo.departmentservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "DepartmentDto Model Information"
+)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DepartmentDto {
     private Long id;
+
+    @Schema(
+            description = "this is the name of the department"
+    )
     private String departmentName;
+    @Schema(
+            description = "this is the description of the department"
+    )
     private String departmentDescription;
+    @Schema(
+            description = "this is the unique department code"
+    )
     private String departmentCode;
 }
