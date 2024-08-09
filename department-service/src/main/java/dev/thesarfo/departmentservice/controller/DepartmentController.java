676a897b54c93d7ab154b00416c44e1b1be3ba5b
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-08-09 19:12:49 +0000

    customize api docs

--- department-service/src/main/java/dev/thesarfo/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/dev/thesarfo/departmentservice/controller/DepartmentController.java
@@ -2,6 +2,9 @@ package dev.thesarfo.departmentservice.controller;
 
 import dev.thesarfo.departmentservice.dto.DepartmentDto;
 import dev.thesarfo.departmentservice.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "Department Service - Department Controller",
+        description = "Department Controller Exposes API endpoints for Department-Service"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/departments")
@@ -19,12 +26,28 @@ public class DepartmentController {
 
     private DepartmentService departmentService;
 
+    @Operation(
+            summary = "Save department",
+            description = "This endpoint is used to save a department in the db"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
         DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
         return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get department",
+            description = "This endpoint is used to get a department object from the db"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 OK"
+    )
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
         DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
