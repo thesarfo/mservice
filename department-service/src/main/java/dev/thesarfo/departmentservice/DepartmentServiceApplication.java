b626119f6652f7222010ebf4733bd036114d1ae9
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-08-09 18:59:19 +0000

    provide general api docs with annotations

--- department-service/src/main/java/dev/thesarfo/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/dev/thesarfo/departmentservice/DepartmentServiceApplication.java
@@ -1,8 +1,33 @@
 package dev.thesarfo.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Department Service APIs",
+                description = "Department Service APIs Documentations",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Ernest",
+                        email = "thesarfo@gmail.com",
+                        url = "https://github.com/thesarfo"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://github.com/thesarfo"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Department-Service Doc",
+                url = "https://github.com/thesarfo"
+        )
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
