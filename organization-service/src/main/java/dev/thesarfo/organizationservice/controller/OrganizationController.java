048fd591dad6ae01cc869ff1f43ed491d0ad43f2
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-08-10 11:37:44 +0000

    document organization service

--- organization-service/src/main/java/dev/thesarfo/organizationservice/controller/OrganizationController.java
+++ organization-service/src/main/java/dev/thesarfo/organizationservice/controller/OrganizationController.java
@@ -3,6 +3,8 @@ package dev.thesarfo.organizationservice.controller;
 import dev.thesarfo.organizationservice.dto.OrganizationDto;
 import dev.thesarfo.organizationservice.entity.Organization;
 import dev.thesarfo.organizationservice.service.OrganizationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +22,28 @@ public class OrganizationController {
 
     private OrganizationService organizationService;
 
+    @Operation(
+            summary = "Save organization",
+            description = "This endpoint is used to save an organization in the db"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<OrganizationDto> saveOrganization(@RequestBody OrganizationDto organizationDto) {
         OrganizationDto savedOrganization = organizationService.saveOrganization(organizationDto);
         return new ResponseEntity<>(savedOrganization, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get organization",
+            description = "This endpoint is used to get an organization object from the db using an organization code"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 OK"
+    )
     @GetMapping("{code}")
     public ResponseEntity<OrganizationDto> getOrganization(@PathVariable("code") String organizationCode){
         OrganizationDto organizationDto = organizationService.getOrganizationByCode(organizationCode);
--- organization-service/src/main/java/dev/thesarfo/organizationservice/dto/OrganizationDto.java
+++ organization-service/src/main/java/dev/thesarfo/organizationservice/dto/OrganizationDto.java
@@ -1,5 +1,6 @@
 package dev.thesarfo.organizationservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,14 +8,25 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Schema(
+        description = "OrganizationDto Model Information"
+)
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrganizationDto {
     private Long id;
+
+    @Schema(
+            description = "this is the name of the organization"
+    )
     private String organizationName;
     private String organizationDescription;
+
+    @Schema(
+            description = "this is the unique organization code"
+    )
     private String organizationCode;
     private LocalDateTime organizationCreationDate;
 }
